821f7cbe2ad5cdea0408c43bba0dec33c5308ee8
Mardon <mardonfayzullaev2006@gmail.com>
2024-06-09 13:33:53 +0300

    modified:   src/App.js 	new file:   src/components/Clicker/Clicker.jsx

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './hooks/useTelegram';
 import Header from './components/Header/Header';
+import Clicker from './components/Clicker/Clicker';
 
 
 function App() {
   const {onToggleButton, tg} = useTelegram();
-
 
   useEffect(() => {
     tg.ready();
@@ -15,6 +15,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Clicker />
       <button onClick={onToggleButton}>toggle</button>
     </div>
   );
