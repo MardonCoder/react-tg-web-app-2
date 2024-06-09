aa321dbcc57a3ee131e0fbf2006410555d45101e
Mardon <mardonfayzullaev2006@gmail.com>
2024-06-09 12:11:54 +0300

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -12,19 +12,8 @@ function App() {
     tg.close()
   }
 
-  const Clicker = () => {
-    const [count, setCount] = useState(0);
-  
-    const handleClick = () => {
-      setCount(count + 1);
-    };
-  }
-
   return (
     <div className="App">
-      <h1>Clicker App</h1>
-      <p>Count: {count}</p>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk3Ylt99YuoPEDlF838eaMID6VsHgQiO_eGg&s" alt="Clicker" onClick={handleClick} style={{ cursor: 'pointer' }} />
       work
       <button onClick = {onClose}>Закрыть</button>
     </div>
